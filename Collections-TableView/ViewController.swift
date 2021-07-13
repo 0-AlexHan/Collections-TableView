5f9b881a2cd77d9515be9590ddf851083d709aae
Alexhan <alexandrganin0@gmail.com>
2021-07-13 18:12:53 +0300

    Initial Commit

---
+++ Collections-TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collections-TableView
+//
+//  Created by Alex Han on 13.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
